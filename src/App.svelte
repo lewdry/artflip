b935ece3a7926331055196eabba9a26bbb299b38
lewdry <30194191+lewdry@users.noreply.github.com>
2025-09-30 16:40:45 +1000

    refactor: update header subtitle

--- src/App.svelte
+++ src/App.svelte
@@ -111,7 +111,7 @@
     <header>
       <div class="title-group">
         <h1>ArtFlip</h1>
-        <h2>Random highlights from the Metropolitan Museum of Art.</h2>
+        <h2>Random artworks from open access collections.</h2>
       </div>
 
       <button on:click={fetchRandomArtwork} disabled={loading} class="refresh-btn">
