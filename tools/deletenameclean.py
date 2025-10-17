95dc753016a878004faa60bc1869b34ac7e65feb
lewdry <30194191+lewdry@users.noreply.github.com>
2025-10-18 02:01:22 +1100

    Deleted dud images.

--- tools/deletenameclean.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open("delete.json") as f:
-    data = [line.strip().replace('.jpg', '') for line in f if line.strip()]
-
-with open("output.json", "w") as f:
-    json.dump(data, f, indent=2)
